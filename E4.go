609b5c188aba86ad9046419879e4b35e1d0d67a1
Ahsan Abbas <ahsan4401360@gmail.com>
2021-05-28 20:54:18 +0500

    EasyQ1

---
+++ E4.go
@@ -0,0 +1,18 @@
+package main
+
+// import "fmt"
+
+// func CnvrtToAcrnym(s string) string {
+// 	var b string
+// 	for i := range s {
+// 		if s[i] >= 'A' && s[i] <= 'Z' {
+// 			b = b + string(s[i])
+// 		}
+
+// 	}
+// 	return b
+// }
+
+// func main() {
+// 	fmt.Println(CnvrtToAcrnym("Enter your Sentence"))
+// }
