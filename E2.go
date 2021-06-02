dee756ab268c3d4246add9f98594be2c7f23a80d
Ahsan Abbas <ahsan4401360@gmail.com>
2021-06-03 01:48:32 +0500

    Q2Golang

---
+++ E2.go
@@ -0,0 +1,15 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// 	"time"
+// )
+
+// func GigaAdd() time.Time {
+// 	return time.Unix(int64(float64(time.Now().Unix())+math.Pow(10, 9)), 0)
+
+// }
+// func main() {
+// 	fmt.Println(GigaAdd())
+// }
